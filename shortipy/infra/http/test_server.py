1e15c596e7c8687c2d9e3018ded3eb07d351211b
ifilipe-lype <ifilipe.lype@gmail.com>
2022-08-02 09:53:13 +0100

    feat: creates bare fastapi server

---
+++ shortipy/infra/http/test_server.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from .server import app
+
+
+def test_ping_pong():
+    client = TestClient(app)
+
+    response = client.get('/ping')
+
+    assert response.status_code == 200
+    assert response.json() == { 'msg' : 'pong'}
