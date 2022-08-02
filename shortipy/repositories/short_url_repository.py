d869d2ca3b6802bce4463523991604579086cfc7
ifilipe-lype <ifilipe.lype@gmail.com>
2022-08-02 09:52:37 +0100

    fix: fixes imports

--- shortipy/repositories/short_url_repository.py
+++ shortipy/repositories/short_url_repository.py
@@ -1,4 +1,4 @@
-from entities.short_url import ShortURL
+from shortipy.entities.short_url import ShortURL
 
 class ShortURLsRepository:
     async def create(self, short_url: ShortURL) -> ShortURL :
--- shortipy/tests/fixtures/repositories/short_url_repository_in_memory.py
+++ shortipy/tests/fixtures/repositories/short_url_repository_in_memory.py
@@ -1,6 +1,6 @@
 from venv import create
-from repositories.short_url_repository import ShortURLsRepository
-from entities.short_url import ShortURL
+from shortipy.repositories.short_url_repository import ShortURLsRepository
+from shortipy.entities.short_url import ShortURL
 
 
 class ShortURLRepositoryInMemory(ShortURLsRepository):
