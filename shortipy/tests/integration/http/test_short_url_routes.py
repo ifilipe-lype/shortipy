56b92687a67f8b96ff28156d5c97d022135bd32a
ifilipe-lype <ifilipe.lype@gmail.com>
2022-08-07 22:33:57 +0100

    refactor: moves integration tests to better place

--- shortipy/tests/integration/http/test_short_url_routes.py
+++ shortipy/tests/integration/http/test_short_url_routes.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from shortipy.tests.fixtures.fake_short_url import make_fake_shortURL
 from shortipy.tests.fixtures.repositories.short_url_repository_in_memory import ShortURLRepositoryInMemory
 
-from ..server import app
+from shortipy.infra.http.server import app
 
 
 class TestShortURLRoutes:
