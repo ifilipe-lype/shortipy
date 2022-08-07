c1e17165b63ee4bba9625664b4bb91e1494d20e6
ifilipe-lype <ifilipe.lype@gmail.com>
2022-08-07 22:24:14 +0100

    feat: redirects user to target_url based on access key

--- shortipy/infra/http/routes/short_url_routes.py
+++ shortipy/infra/http/routes/short_url_routes.py
@@ -1,19 +1,23 @@
-from dataclasses import dataclass
 import os
+from dataclasses import dataclass
+
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import RedirectResponse
 
 from shortipy.entities.short_url import ShortURL
 from shortipy.entities.app_error import AppError
 from shortipy.infra.database.repositories.shorturl_redis_repository import ShortURLsRedisRepository
 from shortipy.usecases.create_shorturl_usecase import CreateShortURLUseCase
+from shortipy.usecases.get_shorturl_by_key_usecase import GetShortURLByKeyUsecase
 
 router = APIRouter(
     tags=['short_urls'],
     responses={400: {'description': 'Invalid url!'}}
 )
 
-redisShortURLsRepo = ShortURLsRedisRepository()
-createShortURLUseCase = CreateShortURLUseCase(redisShortURLsRepo)
+repo = ShortURLsRedisRepository()
+createShortURLUseCase = CreateShortURLUseCase(repo)
+getShortURLByKeyUseCase = GetShortURLByKeyUsecase(repo)
 
 
 @dataclass
@@ -29,5 +33,15 @@ async def create_short_url(payload: CreateShortURLPayload):
         short_url.access_key = f"{os.environ.get('REDIRECTION_BASE_URL')}/{short_url.access_key}"
 
         return short_url
+    except AppError as e:
+        raise HTTPException(status_code=e.code, detail=e.msg)
+
+
+@router.get('/{access_key}')
+async def redirectByAccessKey(access_key: str):
+    '''redirects user to target url based on access_key'''
+    try:
+        short_url = await getShortURLByKeyUseCase.execute(access_key)
+        return RedirectResponse(short_url.target_url)
     except AppError as e:
         raise HTTPException(status_code=e.code, detail=e.msg)
