7b76ed5bcc2e14501cd6c70e796f357f44c2ec3a
ifilipe-lype <ifilipe.lype@gmail.com>
2022-08-07 14:30:06 +0100

    feat: includes other routes after ping route declaration

--- shortipy/infra/http/server.py
+++ shortipy/infra/http/server.py
@@ -5,10 +5,10 @@ from .routes.short_url_routes import router as short_url_router
 
 app = FastAPI()
 
-app.include_router(short_url_router)
 
 @app.get('/ping')
 async def ping_pong():
     return { 'msg': 'pong'}
 
 
+app.include_router(short_url_router)
